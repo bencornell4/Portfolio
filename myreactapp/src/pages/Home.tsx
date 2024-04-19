7bde00559aec918bea02d50bcbd74387ef38a24c
bencornell4 <bencornell44@gmail.com>
2024-04-19 14:46:24 -0700

    Refactored Pages
    Created a page component that all choice-centered pages share

--- myreactapp/src/pages/Home.tsx
+++ myreactapp/src/pages/Home.tsx
@@ -1,95 +1,47 @@
-import React, {useState, useEffect} from "react";
-import { useNavigate } from "react-router-dom";
-import { motion } from 'framer-motion';
+import React, {useContext} from "react";
 import Typewriter from "@components/Typewriter";
 import FadeIn from "@components/FadeIn";
 import Delay from "@components/Delay";
 import AnimatedButton from "@components/AnimatedButton";
+import PageComponentContext from "@components/PageComponentContext";
 
 const Home: React.FC = () => {
-    const [choiceJob, setChoiceJob] = useState<string>(". . .");
-    const [fadeOut, setFadeOut] = useState<boolean>(false);
     const gradientText = "bg-gradient-to-tr from-blue-400 to-blue-800 text-transparent bg-clip-text";
-    const navigate = useNavigate();
-
-
-    const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        var typeDelay = 0;
-        var path = "/";
-        const choice = event.currentTarget.textContent;
-        
-        //fade out buttons
-        const buttons = Array.from(document.getElementsByClassName('choice-button'));
-        for (const button of buttons) {
-            if (button instanceof HTMLElement) {
-                button.style.opacity = '0'; // Set opacity to 0 (fully transparent)
-                button.style.transition = 'opacity 0.5s ease-out'; // Apply transition to opacity
-                button.setAttribute('disabled', '');
-            }
-        }
-
-        if (choice == `Writer`) {
-            path = "/writer";
-            typeDelay = 1000;
-            setChoiceJob(choice || ". . .");
-        }
-        else if (choice == `Fullstack Developer`) {
-            path = "/fullstack";
-            typeDelay = 2500;
-            setChoiceJob(choice || ". . .");
-        }
-        
-        const timeoutId = setTimeout(() => {
-            setFadeOut(true);
-            navigatePage(path);
-        }, typeDelay);
-    };
-
-    function navigatePage(path: string) {
-        const timeoutId = setTimeout(() => {
-            navigate(path);
-        }, 1000);
-    }
-
+    const {choiceJob, handleButtonClick} = useContext(PageComponentContext);
+    const choices =
+    [
+        ["Fullstack Developer", "/fullstack"],
+    ]
 
     return (
-        <motion.div
-            initial={{ opacity: 0, y: "50%" }} // Initial position (below the viewport)
-            animate={{ opacity: 1, y: !fadeOut ? "0%" : "-100%"}} // Final position (slide up into view)
-            exit={{ opacity: 0, y: "-50%" }} // Exit animation (slide up and fade out)
-            transition={{ duration: 1 }} // Animation duration
-        >
             <div className="container px-4 h-screen font-sans text-white mx-auto">
-                <div>
-                    <div className="flex items-center h-screen">
-                        <div className="min-w-[11rem] mx-auto text-2xl font-bold">
-                            <Typewriter className="mt-8 inline-flex" delay={4}>I'm&nbsp;</Typewriter>
-                            <Typewriter className={"inline-flex " + gradientText} delay={8}>Ben Cornell</Typewriter>
-                            <div>
-                                <Typewriter className="inline-flex" delay={24}>I'm a&nbsp;</Typewriter>
-                                <Delay className="inline-flex" duration={34}>
-                                    <Typewriter key={choiceJob} className={gradientText}>{choiceJob}</Typewriter>
-                                </Delay>
-                            </div>
-                            <FadeIn className="mt-10" delay={30}>
-                                <div className="text-center">
-                                    <div>
-                                        <AnimatedButton onClick={(e) => handleButtonClick(e)} className="choice-button text-base border-2 p-2 rounded-lg" mode="color" textColor="hover:text-blue-400" borderColor="hover:border-blue-400">
-                                            Fullstack Developer
-                                        </AnimatedButton>    
-                                    </div>
-                                    {/*<div className="mt-4">
-                                        <AnimatedButton onClick={(e) => handleButtonClick(e)} className="choice-button text-base border-2 p-2 rounded-lg" mode="color" textColor="hover:text-blue-400" borderColor="hover:border-blue-400">
-                                            Writer
-                                        </AnimatedButton>
-                                    </div>*/}
-                                </div>
-                            </FadeIn>
+                <div className="flex items-center h-screen">
+                    <div className="min-w-[11rem] mx-auto text-2xl font-bold">
+                        <Typewriter className="mt-8 inline-flex" delay={4}>I'm&nbsp;</Typewriter>
+                        <Typewriter className={"inline-flex " + gradientText} delay={8}>Ben Cornell</Typewriter>
+                        <div>
+                            <Typewriter className="inline-flex" delay={24}>I'm a&nbsp;</Typewriter>
+                            <Delay className="inline-flex" duration={34}>
+                                <Typewriter key={choiceJob} className={gradientText}>{choiceJob}</Typewriter>
+                            </Delay>
                         </div>
+                        <FadeIn className="mt-10" delay={30}>
+                            <div className="text-center">
+                                <div>
+                                    <AnimatedButton onClick={(e) => handleButtonClick(choices[0][0], choices)} className="choice-button text-base border-2 p-2 rounded-lg" mode="color" textColor="hover:text-blue-400" borderColor="hover:border-blue-400">
+                                        Fullstack Developer
+                                    </AnimatedButton>    
+                                </div>
+                                {/*<div className="mt-4">
+                                    <AnimatedButton onClick={(e) => handleButtonClick("Writer", choices)} className="choice-button text-base border-2 p-2 rounded-lg" mode="color" textColor="hover:text-blue-400" borderColor="hover:border-blue-400">
+                                        Writer
+                                    </AnimatedButton>
+                                </div>*/}
+                            </div>
+                        </FadeIn>
                     </div>
                 </div>
             </div>
-        </motion.div>
     );
 };
 
--- myreactapp/src/pages/Writer.tsx
+++ myreactapp/src/pages/Writer.tsx
@@ -1,110 +1,60 @@
-import React, {useState, useEffect} from "react";
+import React, { useContext } from "react";
 import Typewriter from "@components/Typewriter";
 import FadeIn from "@components/FadeIn";
 import Delay from "@components/Delay";
-import { motion } from "framer-motion";
-import { useNavigate } from "react-router-dom";
 import AnimatedButton from "@components/AnimatedButton";
+import PageComponentContext from "@components/PageComponentContext";
 
 const Fullstack: React.FC = () => {
-    const [choiceJob, setChoiceJob] = useState<string>(". . .");
-    const [fadeOut, setFadeOut] = useState<boolean>(false);
     const gradientText = "bg-gradient-to-tr from-blue-800 to-blue-400 text-transparent bg-clip-text";
-    const navigate = useNavigate();
-
-
-    const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        var typeDelay = 0;
-        var path = "/";
-        const choice = event.currentTarget.textContent;
-        
-        //fade out buttons
-        const buttons = Array.from(document.getElementsByClassName('choice-button'));
-        for (const button of buttons) {
-            if (button instanceof HTMLElement) {
-                button.style.opacity = '0'; // Set opacity to 0 (fully transparent)
-                button.style.transition = 'opacity 0.5s ease-out'; // Apply transition to opacity
-                button.setAttribute('disabled', '');
-            }
-        }
-
-        if (choice == `"All Just Debts"`) {
-            path = "/writer/alljustdebts";
-            typeDelay = 2500;
-            setChoiceJob(choice || ". . .");
-        }
-        else if (choice == `"Anything I like?"`) {
-            path = "/writer/anythingilike";
-            typeDelay = 2500;
-            setChoiceJob(choice || ". . .");
-        }
-        else if (choice == `"Piss drunk, too"`) {
-            path = "/writer/pissdrunktoo";
-            typeDelay = 2500;
-            setChoiceJob(choice || ". . .");
-        }
-        
-        const timeoutId = setTimeout(() => {
-            setFadeOut(true);
-            navigatePage(path);
-        }, typeDelay);
-
-        return () => clearTimeout(timeoutId);
-    };
-
-    function navigatePage(path: string) {
-        const timeoutId = setTimeout(() => {
-            navigate(path);
-        }, 1000);
-    }
+    const {choiceJob, handleButtonClick} = useContext(PageComponentContext);
+    const choices =
+    [
+        ["All Just Debts", "/writer/alljustdebts"],
+        ["Anything I like?", "/writer/anythingilike"],
+        ["Piss drunk, too", "/writer/pissdrunktoo"],
+    ]
 
     return (
-        <motion.div
-            initial={{ opacity: 0, y: "50%" }} // Initial position (below the viewport)
-            animate={{ opacity: 1, y: !fadeOut ? "0%" : "-100%"}} // Final position (slide up into view)
-            exit={{ opacity: 0, y: "-50%" }} // Exit animation (slide up and fade out)
-            transition={{ duration: 1 }} // Animation duration
-        >
+        <div>
             <div className={'fixed top-0 left-2 mx-auto text-base font-bold p-4 z-50 font-mono '}>
                 <h2 className="inline-flex">bencornell</h2>
-                <AnimatedButton onClick={(e) => handleButtonClick(e)} className={gradientText} mode="size">
+                <AnimatedButton onClick={(e) => handleButtonClick("back", choices)} className={gradientText} mode="size">
                     /writer
                 </AnimatedButton>
             </div>
             <div className="bg-gradient-to-tr from-gray-100 to-gray-50 px-4 h-screen w-full font-mono text-gray-800 mx-auto">
-                <div>
-                    <div className="flex items-center h-screen">
-                        <div className="mx-auto text-2xl font-bold whitespace-nowrap">
-                            <div>
-                                <Typewriter className="inline-flex" delay={4}>I wrote&nbsp;</Typewriter>
-                                <Delay className="inline-flex" duration={14}>
-                                    <Typewriter key={choiceJob} className={gradientText}>{choiceJob}</Typewriter>
-                                </Delay>
-                            </div>
-                            <FadeIn className="mt-10" delay={18}>
-                                <div className="text-center">
-                                    <div>
-                                        <AnimatedButton onClick={(e) => handleButtonClick(e)} className="choice-button text-base" mode="color" textColor="hover:text-blue-700">
-                                            "All Just Debts"
-                                        </AnimatedButton>
-                                    </div>
-                                    <div className="mt-4">
-                                        <AnimatedButton onClick={(e) => handleButtonClick(e)} className="choice-button text-base" mode="color" textColor="hover:text-blue-700">
-                                            "Anything I like?"
-                                        </AnimatedButton>
-                                    </div>
-                                    <div className="mt-4">
-                                        <AnimatedButton onClick={(e) => handleButtonClick(e)} className="choice-button text-base" mode="color" textColor="hover:text-blue-700">
-                                            "Piss drunk, too"
-                                        </AnimatedButton>
-                                    </div>
-                                </div>
-                            </FadeIn>
+                <div className="flex items-center h-screen">
+                    <div className="mx-auto text-2xl font-bold whitespace-nowrap">
+                        <div>
+                            <Typewriter className="inline-flex" delay={4}>I wrote&nbsp;</Typewriter>
+                            <Delay className="inline-flex" duration={14}>
+                                <Typewriter key={choiceJob} className={gradientText}>{choiceJob}</Typewriter>
+                            </Delay>
                         </div>
+                        <FadeIn className="mt-10" delay={18}>
+                            <div className="text-center">
+                                <div>
+                                    <AnimatedButton onClick={(e) => handleButtonClick(choices[0][0], choices)} className="choice-button text-base" mode="color" textColor="hover:text-blue-700">
+                                        "All Just Debts"
+                                    </AnimatedButton>
+                                </div>
+                                <div className="mt-4">
+                                    <AnimatedButton onClick={(e) => handleButtonClick(choices[1][0], choices)} className="choice-button text-base" mode="color" textColor="hover:text-blue-700">
+                                        "Anything I like?"
+                                    </AnimatedButton>
+                                </div>
+                                <div className="mt-4">
+                                    <AnimatedButton onClick={(e) => handleButtonClick(choices[2][0], choices)} className="choice-button text-base" mode="color" textColor="hover:text-blue-700">
+                                        "Piss drunk, too"
+                                    </AnimatedButton>
+                                </div>
+                            </div>
+                        </FadeIn>
                     </div>
                 </div>
             </div>
-        </motion.div>
+        </div>
     );
 };
 
